937a0e850f5857bd53395b7142fd8bdc8b053201
nedodn <odennate@gmail.com>
2020-04-02 16:23:59 -0500

    oracle

---
+++ contracts/Oracle.sol
@@ -0,0 +1,194 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+interface IERC20Token {
+	function balanceOf(address) external pure returns (uint256);
+}
+
+interface UniswapFactory {
+    function getExchange(IERC20Token) external pure returns (address);
+}
+
+interface BancorConverter {
+    struct tokenInfo {
+        uint256 virtualBalance;
+        uint32 ratio;
+        bool isVirtualBalanceEnabled;
+        bool isSaleEnabled;
+        bool isSet;
+    }
+
+    function connectors(IERC20Token) external pure returns (tokenInfo memory);
+    function conversionFee() external pure returns (uint32);
+}
+
+interface SynthetixDepot {
+    function totalSellableDeposits() external pure returns (uint256);
+}
+
+interface SynthetixRates {
+    function rateForCurrency(bytes32) external pure returns (uint256);
+}
+
+interface CurveExchange {
+    function balances(int128) external pure returns (uint256);
+    function A() external pure returns (uint256);
+}
+
+interface yToken {
+    function getPricePerFullShare() external pure returns (uint256);
+}
+
+interface MatchingMarket {
+    function offers(uint256 id) external view returns(uint256 pay_amt, address pay_gem, uint256 buy_amt, address buy_gem, address owner, uint64 timestamp);
+    function getOfferCount(address base, address quote) external view returns(uint256 count);
+    function getBestOffer(address base, address quote) external view returns(uint256 id);
+    function getWorseOffer(uint256 id) external view returns(uint256 worseOfferId);
+}
+
+contract Oracle {
+    struct uniswapData {
+        uint256 tokenBalance;
+        uint256 ethBalance;
+    }
+    UniswapFactory public uniFactory = UniswapFactory(0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95);
+    function getUniswapData(IERC20Token[] memory tokens) public view returns (uniswapData[] memory) {
+        uint256 length = tokens.length;
+		uniswapData[] memory data = new uniswapData[](length);
+		for (uint256 i = 0; i < length; ++i) {
+            IERC20Token token = tokens[i];
+            address exchange = uniFactory.getExchange(token);
+			uint256 tokenBalance = token.balanceOf(exchange);
+			uint256 ethBalance = exchange.balance;
+            data[i] = uniswapData({
+                tokenBalance: tokenBalance,
+                ethBalance: ethBalance
+            });
+		}
+		return data;
+	}
+
+    struct bancorData {
+        uint32 eWeight;
+        uint32 tWeight;
+        uint32 tFee;
+        uint256 tBalance;
+        uint256 eBalance;
+    }
+    IERC20Token public BNT = IERC20Token(0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C);
+    function getBancorData(IERC20Token[] memory tokens, BancorConverter[] memory converters) public view returns (bancorData[] memory) {
+        uint256 length = tokens.length;
+        bancorData[] memory data = new bancorData[](length);
+        for (uint256 i = 0; i < length; ++i) {
+            BancorConverter converter = converters[i];
+            BancorConverter.tokenInfo memory info = converter.connectors(tokens[i]);
+            uint32 eWeight = info.ratio;
+            uint32 tWeight = 1000000 - eWeight;
+            uint32 tFee = converter.conversionFee();
+            uint256 tBalance = tokens[i].balanceOf(address(converter));
+            uint256 eBalance = BNT.balanceOf(address(converter));
+            data[i] = bancorData({
+                eWeight: eWeight,
+                tWeight: tWeight,
+                tFee: tFee,
+                tBalance: tBalance,
+                eBalance: eBalance
+            });
+        }
+        return data;
+    }
+
+    struct synthetixData {
+        uint256 rate;
+        uint256 balance;
+    }
+    SynthetixDepot public Depot = SynthetixDepot(0xE1f64079aDa6Ef07b03982Ca34f1dD7152AA3b86);
+    SynthetixRates public Rates = SynthetixRates(0x9D7F70AF5DF5D5CC79780032d47a34615D1F1d77);
+    function getSynthetixData() public view returns (synthetixData memory) {
+        bytes32 ethKey = 0x4554480000000000000000000000000000000000000000000000000000000000;
+        uint256 rate = Rates.rateForCurrency(ethKey);
+        uint256 balance = Depot.totalSellableDeposits();
+        return synthetixData({
+            rate: rate,
+            balance: balance
+        });
+    }
+
+    struct curveData {
+        uint256 balance;
+        uint256 pricePerShare;
+        uint256 A;
+    } 
+    CurveExchange public Exchange = CurveExchange(0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51);
+    function getCurveData(int128[] memory ids, yToken[] memory tokens) public view returns (curveData[] memory) {
+        uint256 length = ids.length;
+        curveData[] memory data = new curveData[](length);
+        for (uint256 i = 0; i < length; ++i) {
+            uint256 balance = Exchange.balances(ids[i]);
+            uint256 pricePerShare = tokens[i].getPricePerFullShare();
+            uint256 A = Exchange.A();
+            data[i] = curveData({
+                balance: balance,
+                pricePerShare: pricePerShare,
+                A: A
+            });
+        }
+        return data;
+    }
+
+    struct Offer {
+        uint256 id;
+        address maker;
+        uint256 makerAmount;
+        uint256 takerAmount;
+    }
+
+    struct oasisData {
+        Offer[] bids;
+        Offer[] asks;
+    }
+    MatchingMarket oasisMarket = MatchingMarket(0x794e6e91555438aFc3ccF1c5076A74F42133d08D);
+
+    function getOasisData(address[] memory bases, address[] memory quotes) public view returns (oasisData[] memory) {
+        uint256 length = bases.length;
+        oasisData[] memory data = new oasisData[](length);
+        for (uint256 i = 0; i < length; ++i) {
+            (Offer[] memory bids, Offer[] memory asks) = getOrderBook(bases[i], quotes[i]);
+            data[i] = oasisData({
+                bids: bids,
+                asks: asks
+            });
+        }
+        return data;
+    }
+
+    function getOrderBook(address base, address quote) public view returns(Offer[] memory bids, Offer[] memory asks) {
+        uint256 offerId;
+        uint256 bidCount = oasisMarket.getOfferCount(quote, base);
+        bids = new Offer[](bidCount);
+        offerId = oasisMarket.getBestOffer(quote, base);
+        bids[0] = getOffer(offerId);
+        for (uint256 i = 1; i < bidCount; i++) {
+            offerId = oasisMarket.getWorseOffer(offerId);
+            bids[i] = getOffer(offerId);
+        }
+        uint256 askCount = oasisMarket.getOfferCount(base, quote);
+        asks = new Offer[](askCount);
+        offerId = oasisMarket.getBestOffer(base, quote);
+        asks[0] = getOffer(offerId);
+        for (uint256 i = 1; i < askCount; i++) {
+            offerId = oasisMarket.getWorseOffer(offerId);
+            asks[i] = getOffer(offerId);
+        }
+    }
+
+        
+    function getOffer(uint256 id) private view returns(Offer memory offer) {
+        (uint256 pay_amt, , uint256 buy_amt, , address owner, ) = oasisMarket.offers(id);
+        offer = Offer(id, owner, pay_amt, buy_amt);
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,103 @@
+const Web3 = require("web3");
+const OracleAbi = require("../build/contracts/Oracle.json");
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://mainnet.infura.io/v3/db0babc871d74cf79895319a8704666c"
+  )
+);
+
+const OracleAddress = "0x6F5EadF6868A6a5003985B2B01a55bb53bf0E0fE";
+
+const Oracle = new web3.eth.Contract(OracleAbi.abi, OracleAddress);
+
+runTests();
+
+async function runTests() {
+  try {
+    await getSynthetixData();
+    await getUniswapData();
+    await getBancorData();
+    await getOasisData();
+    await getCurveData()
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getSynthetixData() {
+  const synthetixData = await Oracle.methods.getSynthetixData().call();
+  console.log("Synthetix: ", synthetixData);
+}
+
+async function getUniswapData() {
+  let tokens = [
+    "0x6b175474e89094c44da98b954eedeac495271d0f",
+    "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+    "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+    "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    "0x06AF07097C9Eeb7fD685c692751D5C66dB49c215"
+  ];
+  const uniswapData = await Oracle.methods.getUniswapData(tokens).call();
+  console.log("Uniswap");
+  for (let i = 0; i < tokens.length; i++) {
+    console.log(tokens[i], uniswapData[i]);
+  }
+}
+
+async function getBancorData() {
+  const tokens = [
+    "0x6b175474e89094c44da98b954eedeac495271d0f",
+    "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+    "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+    "0x0d8775f648430679a709e98d2b0cb6250d2887ef"
+  ];
+  const converters = [
+    "0xd99b0EFeeA095b87C5aD8BCc8B955eD5Ca5Ba146",
+    "0xA2cAF0d7495360CFa58DeC48FaF6B4977cA3DF93",
+    "0x220C400bC0a4347276b432843Cf293F1faC6762a",
+    "0xBd19F30adDE367Fe06c0076D690d434bF945A8Fc"
+  ];
+  const bancorData = await Oracle.methods
+    .getBancorData(tokens, converters)
+    .call();
+  console.log("Bancor");
+  for (let i = 0; i < tokens.length; i++) {
+    console.log(tokens[i], bancorData[i]);
+  }
+}
+
+async function getCurveData() {
+  const ids = ["0", "1", "2", "3"];
+  const yTokens = [
+    "0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01",
+    "0xd6aD7a6750A7593E092a9B218d66C0A814a3436e",
+    "0x83f798e925BcD4017Eb265844FDDAbb448f1707D",
+    "0x73a052500105205d34Daf004eAb301916DA8190f"
+  ];
+  const curveData = await Oracle.methods.getCurveData(ids, yTokens).call();
+  console.log("Curve");
+  for (let i = 0; i < ids.length; i++) {
+    console.log(ids[i], curveData[i]);
+  }
+}
+
+async function getOasisData() {
+  const bases = [
+    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    "0x6b175474e89094c44da98b954eedeac495271d0f",
+    "0x6b175474e89094c44da98b954eedeac495271d0f"
+  ];
+  const quotes = [
+    "0x6b175474e89094c44da98b954eedeac495271d0f",
+    "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+    "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+    "0xe41d2489571d322189246dafa5ebde1f4699f498"
+  ];
+  console.log("Oasis");
+  const oasisData = await Oracle.methods.getOasisData(bases, quotes).call();
+  for (let i = 0; i < bases.length; i++) {
+    console.log(bases[i], quotes[i], oasisData[i]);
+  }
+}
